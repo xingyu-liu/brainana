3801d95fd30016778a55757ec8a20401bffb39e0
Xingyu Liu <liuxingyu@foxmail.com>
2026-03-07 23:24:05 -0500

    update read the docs

--- tests/test_brainana_docker.sh
+++ tests/test_brainana_docker.sh
@@ -6,8 +6,8 @@ fs_license=/mnt/DataDrive3/xliu/prep_test/freesurfer_license.txt
 # bids_dir=/mnt/DataDrive2/macaque/data_raw/macaque_mri/PRIME-DE/${site}
 # output_dir=/mnt/DataDrive2/macaque/data_preproc/macaque_mri/PRIME-DE_brainana/${site}
 
-# # bids_dir=/mnt/DataDrive3/xliu/prep_test/brainana_test/dataset_easy_downsampled
-# # output_dir=/mnt/DataDrive3/xliu/prep_test/brainana_test/preproc/dataset_easy_downsampled_docker_v2
+# bids_dir=/mnt/DataDrive3/xliu/prep_test/brainana_test/dataset_easy_downsampled_multianat
+# output_dir=/mnt/DataDrive3/xliu/prep_test/brainana_test/preproc/dataset_easy_downsampled_multianat_v3
 
 # docker run --rm -t --gpus all \
 #     -v "$bids_dir":/input \
